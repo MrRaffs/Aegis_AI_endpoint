b936f77f48f46865576d265634826fced13de280
Iyan Z Armanda <catcode0101@gmail.com>
2026-04-29 14:00:48 +0700

    fix: service detail schema on data preprocessing
    - this is temporary solved
    
    - there's missmatch value too while some conditional occur
    
    - for better performance, use enum value for service type and service description with py, csvor even json file
    
    - fetch truck id and facility location data from API istead manual search from datasets. check API repository documentation

--- api/dataPreprocessing.py
+++ api/dataPreprocessing.py
@@ -17,30 +17,38 @@ def clean_record(record):
     
     # TODO: must acccess database, get all records with the same truck id, and check for duplicate
     #or just remove this functionality if it is too complex to implement
-    metadata['duplicated_values'] = df.duplicated().sum() 
+    metadata['duplicated_values'] = int(df.duplicated().sum())
     
     df.drop_duplicates(inplace=True)
     
-    # Split Service Desc
-    df[['service_type', 'service_detail']] = df['service_description'].str.split(' ', expand=True)
-    df.drop('service_description', axis=1, inplace=True)
-    
-    # check if maintenance_type is identical to service_type
-    is_identical = df['maintenance_type'] == df['service_type']
-    print('Is maintenance_type identical to service_type?', is_identical.all())
-        
-    if is_identical.all():
-        drop_cols = ['service_type']
-        df.drop(columns=drop_cols, inplace=True)
-        
-        
-    
+    split_data = df['service_description'].str.split(' ', n=1, expand=True)
+
+    df['service_type'] = split_data[0]
+    if split_data.shape[1] > 1:
+        df['service_detail'] = split_data[1]
+    else:
+        df['service_detail'] = "unknown"
+
+    is_identical = (df['maintenance_type'] == df['service_type']).all()
+    print('Is maintenance_type identical to service_type?', is_identical)
+
     # get cols
-    categorical_cols = ['maintenance_type','service_type','facility_location']
+    categorical_cols = ['maintenance_type', 'service_type', 'service_detail', 'facility_location']
+    
     numerical_cols = [
-    'odometer_reading', 'labor_hours', 'labor_cost', 'parts_cost',
-    'total_cost', 'downtime_hours', 'days_since_last'
+        'odometer_reading', 'labor_hours', 'labor_cost', 'parts_cost',
+        'total_cost', 'downtime_hours', 'days_since_last'
     ]
+
+    for col in categorical_cols:
+        if col not in df.columns:
+            df[col] = "Unknown"
+    for col in numerical_cols:
+        if col not in df.columns:
+            df[col] = 0.0
+
+    if 'service_description' in df.columns:
+        df.drop('service_description', axis=1, inplace=True)
     
     return df[categorical_cols + numerical_cols], metadata
 
